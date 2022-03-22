ebfd90396d4b1f6088cd54c2aa9ea9cab2eba579
qmuntal <qmuntaldiaz@microsoft.com>
2022-03-22 16:51:55 +0100

    fix TestRandBig OOM

--- cng/rand_test.go
+++ cng/rand_test.go
@@ -23,7 +23,7 @@ func TestRand(t *testing.T) {
 }
 
 func TestRandBig(t *testing.T) {
-	b := make([]byte, 1<<34)
+	b := make([]byte, 1<<32+60)
 	n, err := io.ReadFull(RandReader, b)
 	if err != nil {
 		t.Fatal(err)
