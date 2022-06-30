ce4cf37c4de956cf1424cd64ccba365b4576359d
Quim Muntal <qmuntaldiaz@microsoft.com>
2022-06-30 09:06:36 +0200

    Update cng/big.go
    Co-authored-by: Davis Goodin <dagood@users.noreply.github.com>

--- cng/big.go
+++ cng/big.go
@@ -7,7 +7,8 @@ package cng
 // Go crypto references BigInt unconditionally,
 // even if it is not finally used.
 
-// A BigInt is the raw big-endian bytes from a BigInt.
+// A BigInt is the big-endian bytes from a math/big BigInt.
+// Windows BCrypt accepts this specific data format.
 // This definition allows us to avoid importing math/big.
 // Conversion between BigInt and *big.Int is in cng/bbig.
 type BigInt []byte
