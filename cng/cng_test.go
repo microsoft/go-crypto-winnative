1a79b0867b0e815715e4ca53c99caeb6b4531150
qmuntal <qmuntaldiaz@microsoft.com>
2022-06-30 08:40:46 +0200

    ensure fips mode matches ci expectation

--- cng/cng_test.go
+++ cng/cng_test.go
@@ -9,20 +9,33 @@ package cng_test
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/microsoft/go-crypto-winnative/cng"
 )
 
 func TestMain(m *testing.M) {
-	fips, _ := cng.FIPS()
+	fips, err := cng.FIPS()
+	if err != nil {
+		fmt.Printf("FIPS() error = %v\n", err)
+	}
 	fmt.Println("FIPS enabled:", fips)
 	os.Exit(m.Run())
 }
 
 func TestFIPS(t *testing.T) {
-	_, err := cng.FIPS()
+	enabled, err := cng.FIPS()
 	if err != nil {
 		t.Errorf("FIPS() error = %v", err)
+	}
+	if s := os.Getenv("GO_TEST_FIPS"); s != "" {
+		wantFips, err := strconv.Atoi(s)
+		if err != nil {
+			t.Fatalf("failed to parse $GO_TEST_FIPS = %q as integer: %v", s, err)
+		}
+		if want := (wantFips == 1); enabled != want {
+			t.Errorf("FIPS() = %v, want = %v", enabled, want)
+		}
 	}
 }
