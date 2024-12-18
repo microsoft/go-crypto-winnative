44528fa8698be97eb3163471e9537ed2161a0745
mertakman <mertakman2012@gmail.com>
2024-12-18 14:34:49 +0000

    fix:add tests

--- internal/bcrypt/ntstatus_test.go
+++ internal/bcrypt/ntstatus_test.go
@@ -5,6 +5,13 @@ import "testing"
 func TestNTStatusErrMessage(t *testing.T) {
 
 	StatusWait1 := NTStatus(0x00000001)
+	StatusWait2 := NTStatus(0x00000002)
 
-	t.Error(StatusWait1.Error())
+	if StatusWait1.Error() != "STATUS_WAIT_1" {
+		t.Errorf("Expected STATUS_WAIT_2, got %s", StatusWait2.Error())
+	}
+
+	if StatusWait2.Error() != "STATUS_WAIT_2" {
+		t.Errorf("Expected STATUS_WAIT_2, got %s", StatusWait2.Error())
+	}
 }
